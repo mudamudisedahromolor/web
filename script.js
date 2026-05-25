098993890c17d8c69e3772aae84c4c5503547af5
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-05-25 23:33:32 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById('mobile-menu-btn');
-const mainNavbar = document.querySelector('.main-navbar');
-
-// Ketika tombol hamburger diklik
-menuBtn.addEventListener('click', () => {
-    // Tambah atau hapus class 'aktif' pada navbar merah
-    mainNavbar.classList.toggle('aktif');
-});
