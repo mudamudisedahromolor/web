12d89845144b951747ea55331d4fcbd33a5f95dc
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-05-26 12:06:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Tautan Google Sheets Resmi Milik Anda (Menggunakan ID Asli dari Screenshot Komitmen Anda)
-const baseLinkSheets = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgjFSc953Gb2e53JTQwdKjnU4iCPIPHmeUoPwwNbheBuCOwPOT3Z5csHt2ZcyM3Kx1arstWuDpiB1v/pub?output=tsv";
 const baseLinkSheets = "https://docs.google.com/spreadsheets/d/1vQgjFSc953Gb2e53JTQwdKjnU4iCPIPHmeUoPwwNbheBuCOwPOT3Z5csHt2ZcyM3Kx1arstWuDpiB1v/export?format=tsv";
 
 let dataKeuanganGlobal = [];
